22a53813196d63efbb1a102abc6d0fd8d9fba0f6
Navaf KP <255navaf@gmail.com>
2024-01-07 21:56:39 +0530

    api url updated

--- src/Routes/PrivateRoutes.js
+++ src/Routes/PrivateRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 
+
 // will redirect to admin or user based on user type
 const PrivateRoutes = ({ is_authenticated, isValidUser, redirect, children }) => {
   return (
